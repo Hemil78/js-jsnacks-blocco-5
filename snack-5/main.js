6b0324a6c694a568410b2fe6bc4f7fc90ba5c0df
Hemil78 <zanattahemil@gmail.com>
2021-09-08 16:02:25 +0200

    snack-5 support fabbri

---
+++ snack-5/main.js
@@ -0,0 +1,29 @@
+//Scrivi una funzione che accetti tre argomenti:
+//un array e due numeri (“a” più piccolo di “b” e “b” grande al massimo quanto il numero di elementi dell’array).
+//La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra “a” e “b”
+
+//1. definisco un'array di elementi
+var arrayDefoult = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var numMin = parseInt(prompt("Inserisci un numero minimo"));
+var numMax = parseInt(prompt("Inserisci un numero massimo"));
+
+
+//2. definisco una FUNZIONE LOCALE
+function argThree(arr, numMin, numMax) {
+    let newArray = [];
+
+
+    for(let i = 0; i < arr.length; i++) {
+         if(i >= numMin && i <= numMax){
+            newArray.push(arr[i]);
+         }
+    }
+    
+    
+    
+    return newArray;
+}
+
+
+var array = argThree(arrayDefoult, numMin, numMax);
+console.log(array);
